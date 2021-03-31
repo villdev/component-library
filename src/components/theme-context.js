0d36b54f36d8b0c5e2f6b201e774a496fc038aa2
vill <villdevelop@gmail.com>
2021-03-31 18:17:46 +0530

    fix: started using localStorage for theme mode

--- src/components/theme-context.js
+++ src/components/theme-context.js
@@ -3,11 +3,15 @@ import React, { createContext, useState, useContext } from "react";
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children, userTheme }) {
-  const [currentTheme, setCurrentTheme] = useState("light");
+  const [currentTheme, setCurrentTheme] = useState(
+    JSON.parse(localStorage.getItem("mode")) || "light"
+  );
   const toggleTheme = () => {
-    setCurrentTheme((currentTheme) =>
-      currentTheme === "light" ? "dark" : "light"
-    );
+    setCurrentTheme((currentTheme) => {
+      const newTheme = currentTheme === "light" ? "dark" : "light";
+      localStorage.setItem("mode", JSON.stringify(newTheme));
+      return newTheme;
+    });
   };
   return (
     <ThemeContext.Provider value={{ currentTheme, userTheme, toggleTheme }}>
