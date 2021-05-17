d091cab24b16d6d9382ba57384fead1cdf20a984
vill <villdevelop@gmail.com>
2021-05-17 20:16:23 +0530

    feat: added scroll active sidebar using intersection observer

--- src/components/Component.js
+++ src/components/Component.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useTheme } from "./theme-context";
 import Prism from "prismjs";
 import reactElementToJSXString from "react-element-to-jsx-string";
@@ -13,11 +13,58 @@ export default function Component({ comp }) {
   const currentVariants =
     currentTheme === "light" ? comp.lightVariants : comp.darkVariants;
   const navVariants = currentVariants.map(({ name }) => name);
+
+  const variantRefs = useRef([]);
+  variantRefs.current = [];
+  const [variantStatus, setVariantStatus] = useState(
+    Array(navVariants.length).fill(false)
+  );
+
+  const addToVariantRefs = (el) => {
+    if (el && !variantRefs.current.includes(el)) {
+      variantRefs.current.push(el);
+    }
+  };
+
+  const getRefIndex = (viewEl, viewElements) => {
+    return viewElements.findIndex((el) => el === viewEl);
+  };
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
   const handleCopy = (reactCode) => {
     navigator.clipboard.writeText(
       reactElementToJSXString(reactCode).slice(26, -6)
     );
   };
+
+  const inViewCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const inViewIndex = getRefIndex(entry.target, variantRefs.current);
+        setVariantStatus((prevStatus) => {
+          return prevStatus.map((status, statusIndex) => {
+            if (statusIndex === inViewIndex) return true;
+            else return false;
+          });
+        });
+      }
+      // else {
+      //   const outOfViewIndex = getRefIndex(entry.target, variantRefs.current);
+      //   setVariantStatus((prevStatus) => {
+      //     return prevStatus.map((status, statusIndex) => {
+      //       if (statusIndex === outOfViewIndex) return false;
+      //       else return status;
+      //     });
+      //   });
+      // }
+    });
+  };
+
   const mainContentRef = useRef(null);
   useEffect(() => {
     Prism.highlightAll();
@@ -27,6 +74,20 @@ export default function Component({ comp }) {
     Prism.highlightAll();
     mainContentRef.current.scrollTo(0, 0);
   }, [comp]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(inViewCallback, options);
+
+    variantRefs.current.forEach((variant) => {
+      observer.observe(variant);
+    });
+
+    return () => {
+      variantRefs.forEach((variant) => {
+        observer.unobserve(variant);
+      });
+    };
+  }, [variantRefs]);
 
   return (
     <div className="main-content" ref={mainContentRef}>
@@ -57,7 +118,11 @@ export default function Component({ comp }) {
         )}
 
         {currentVariants.map((variant, i) => (
-          <div className="component-variant-wrapper" key={i}>
+          <div
+            className="component-variant-wrapper"
+            key={i}
+            ref={addToVariantRefs}
+          >
             <div
               id={variant.name.replace(" ", "-")}
               className="component-variant-header direct-link-wrapper"
@@ -106,6 +171,7 @@ export default function Component({ comp }) {
       </div>
       <RightSidebar
         navVariants={navVariants}
+        variantStatus={variantStatus}
         introPresent={comp.intro !== ""}
       />
     </div>
--- src/components/RightSidebar.js
+++ src/components/RightSidebar.js
@@ -2,7 +2,11 @@ import React from "react";
 
 // import "../css/rightSidebar.css";
 
-export default function RightSidebar({ navVariants, introPresent }) {
+export default function RightSidebar({
+  navVariants,
+  variantStatus,
+  introPresent,
+}) {
   return (
     <div className="right-sidebar">
       <div className="right-sidebar-header">On this page</div>
@@ -15,8 +19,16 @@ export default function RightSidebar({ navVariants, introPresent }) {
           )}
           {navVariants.map((nav, i) => {
             return (
-              <li key={i} className="right-sidebar-nav__item">
-                <a href={`#${nav.replace(" ", "-")}`}>{nav}</a>
+              // <li key={i} className="right-sidebar-nav__item">
+              <li key={i} className={"right-sidebar-nav__item "}>
+                <a
+                  className={
+                    variantStatus[i] ? "right-sidebar-nav__item--active" : ""
+                  }
+                  href={`#${nav.replace(" ", "-")}`}
+                >
+                  {nav}
+                </a>
               </li>
             );
           })}
